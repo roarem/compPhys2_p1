3bdcc3f78ade652631ec6341a12f0a73fe9bdf9a
Your Name <you@example.com>
2016-01-28 16:09:03 +0100

    423542

---
+++ makefile.py
@@ -0,0 +1,75 @@
+import subprocess as sub
+import argparse
+
+class Make:
+
+    def __init__(self, root_folder, dev_folders, build_folder):
+        self.root_folder = root_folder+'/'
+        self.dev_folders = dev_folders
+        self.build_folder = self.root_folder+build_folder+'/'
+
+
+    def make(self):
+
+        print('Warming up the compiler')
+        for folder in self.dev_folders:
+            print('Compiles in folder '+folder)
+            folder = self.root_folder+folder+'/'
+            cpile = sub.Popen(['make','-C',folder],stdout=sub.PIPE)
+            cpile.wait()
+            copy = sub.Popen('cp '+folder+'*.o '+self.build_folder,shell=True)
+        print('Done compiling, taking some time off')
+
+    def clean(self):
+
+        for folder in self.dev_folders:
+            folder = self.root_folder+folder+'/'
+            clean = sub.Popen(['make','-C',folder,'clean'])
+            clean.wait() 
+        
+        print('Cleaning up the build folder...')
+        sub.Popen('rm '+self.build_folder+'*.o',shell=True)
+        pritn('Sparkling clean')
+
+
+
+            
+if __name__=='__main__':
+    root_folder = '/home/rat/fys4411/compPhys2_p1'
+    build_folder = 'build'
+    dev_folders = ['wavefunctions']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('clean',nargs='?',default=None, help='Cleans')
+    args = parser.parse_args()
+
+    compiling = Make(root_folder, dev_folders, build_folder)
+
+    if args.clean == None:
+        compiling.make()
+    if args.clean == 'clean':
+        compiling.clean()
+
+
+
+
+
+
+
+#A MAKEFILE EXAMPLE:
+MAKEFILE='''
+CXX=g++
+CXXFLAGS=-Wall -std=c++11 -c
+
+SRCS=$(wildcard *.cpp)
+
+OBJS=$(subst .cpp,.o,$(SRCS))
+
+all:
+	for source in $(SRCS); do \
+	  $(CXX) $(CXXFLAGS) $$source; \
+	done
+
+clean:
+	rm $(OBJS)
+'''
