c0d31f7fd955deda056d190b427b5246ce76cdc7
Your Name <you@example.com>
2016-02-05 09:54:40 +0100

    Created particle class

---
+++ System/Particle.cpp
@@ -0,0 +1,18 @@
+#include "Particle.h"
+
+void Particle::set_nDimensions (int nDimensions)
+{
+  my_nDimensions = nDimensions;
+}
+
+void Particle::set_position (const std::vector<double> &position)
+{
+  my_position = position;
+}
+
+void Particle::changePosition (int dimension, double movement)
+{
+  my_position.at(dimension) += movement;
+}
+
+
