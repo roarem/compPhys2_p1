23a6e29c7abb99bd8095ed012b48ea515ea00bfd
Your Name <you@example.com>
2016-03-10 16:54:16 +0100

    3r8

--- program/Hamiltonian/HarmonicOscillator.cpp
+++ program/Hamiltonian/HarmonicOscillator.cpp
@@ -8,17 +8,21 @@ HarmonicOscillator::HarmonicOscillator (System* system) :
 
 double HarmonicOscillator::computeLocalEnergy ()
 {
+  int	 nD		      = my_system->get_nDimensions();
+  int	 nP		      = my_system->get_nParticles();
   double potEnergy	      = 0;
   double kinEnergy	      = 0;
   double doubleDerivative     = 0;
+  double alpha		      = my_system->get_parameters()[0];
   double omega2		      = my_system->get_parameters()[1]*
 				my_system->get_parameters()[1];
   double gamma2		      = my_system->get_parameters()[2]*
 				my_system->get_parameters()[2];
   double waveFunctionCurrent  = my_system->get_waveFunction()->evaluate();
   
-  for (int p = 0 ; p < my_system->get_nParticles() ; p++){
-    for (int d = 0 ; d < my_system->get_nDimensions() ; d++){
+  
+  for (int p = 0 ; p < nP ; p++){
+    for (int d = 0 ; d < nD ; d++){
       const double x = my_system->get_particle()[p]->get_position()[d];
       if (d==2){
 	potEnergy += x*x*gamma2;
@@ -27,12 +31,14 @@ double HarmonicOscillator::computeLocalEnergy ()
         potEnergy += x*x*omega2;
 
       }
+      //kinEnergy -= (2*alpha*x*x - nD);
       doubleDerivative += my_system->get_waveFunction()->
 			  computeDoubleDerivative(p,d,waveFunctionCurrent);
     }
   }
 
   kinEnergy = -0.5*doubleDerivative/waveFunctionCurrent;
+  //kinEnergy = kinEnergy*alpha;
   potEnergy = 0.5*potEnergy;
 		  
   return kinEnergy + potEnergy;
--- program/WaveFunction/TrialWaveFunctionAna.cpp
+++ program/WaveFunction/TrialWaveFunctionAna.cpp
@@ -13,6 +13,7 @@ double TrialWaveFunctionAna::evaluate ()
 {
   double wave	= 0;
   double r2	= 0;
+  const double alpha = my_system->get_parameters()[0];
 
   for (int i = 0 ; i < my_system->get_nParticles() ; i++){
     for (int j = 0 ; j < my_system->get_nDimensions() ; j++){
@@ -21,7 +22,7 @@ double TrialWaveFunctionAna::evaluate ()
 	    my_system->get_particle()[i]->get_position()[j];
     }
   }
-  wave -= my_system->get_parameters()[0]*r2;
+  wave -= alpha*r2;
 
   return exp(wave);
 }
@@ -31,6 +32,8 @@ double TrialWaveFunctionAna::computeEnergy()
   double kinEnergy	= 0;
   double potEnergy	= 0;
   double r2		= 0;
+  const double alpha = my_system->get_parameters()[0];
+  const double omega = my_system->get_parameters()[1];
   
   for (int i = 0 ; i < my_system->get_nParticles() ; i++){
     for (int j = 0 ; j < my_system->get_nDimensions() ; j++){
@@ -38,13 +41,13 @@ double TrialWaveFunctionAna::computeEnergy()
       r2 =  my_system->get_particle()[i]->get_position()[j]*
 	    my_system->get_particle()[i]->get_position()[j];
       }
-      kinEnergy -= (2*my_system->get_parameters()[0]*r2 - my_system->get_nDimensions());
+      kinEnergy -= (2*alpha*r2 - my_system->get_nDimensions());
       potEnergy += r2;
       //cout << my_system->get_particle()[i]->get_position()[0] << endl;
   }
 
-  kinEnergy = kinEnergy *my_system->get_parameters()[0];
-  potEnergy = my_system->get_parameters()[1] *my_system->get_parameters()[1] * potEnergy * 0.5;
+  kinEnergy = kinEnergy *alpha;
+  potEnergy = omega*omega * potEnergy * 0.5;
   //cout << "potential energy = " << potEnergy << ", kinetic energy = " << kinEnergy << endl;
   return kinEnergy + potEnergy;
 }
