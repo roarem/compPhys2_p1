7cbcef4f7b3acce7703ee23f9f2cdd891f598b7f
Roar Emaus <roarem@fys.uio.no>
2016-03-17 17:05:00 +0100

    3242

--- program/Hamiltonian/HarmonicOscillatorInteracting.cpp
+++ program/Hamiltonian/HarmonicOscillatorInteracting.cpp
@@ -34,7 +34,44 @@ double HarmonicOscillatorInteracting::computeLocalEnergy ()
   }
   return vInt + vExt;
 }
-      
+     
+double HarmonicOscillatorInteracting::correlationFunction ()
+{
+  
+  int nP    = my_system->get_nParticles();
+  int nD    = my_system->get_nDimensions();
+  
+  const double alpha  = my_system->get_parameters()[0];
+  const double beta   = my_system->get_parameters()[3];
+  const double alpha2 = alpha*alpha;
+  const double abterm = 4*alpha - 2*alpha*beta;
+  const double boson  = my_bosonSize;
+  const double boson2 =	my_bosonSize2;
+
+  for (int p1 = 0 ; p1 < nP-1 ; p1++){
+    
+    for (int p2 = p1 + 1 ; p2 < nP ; p2++){
+      const double rij	= 0;
+      const double x1 = my_system->get_particle()[p1]->get_postition()[0];
+      const double x2 = my_system->get_particle()[p1]->get_postition()[0];
+      const double y1 = my_system->get_particle()[p1]->get_postition()[1];
+      const double y2 = my_system->get_particle()[p1]->get_postition()[1];
+      const double z1 = my_system->get_particle()[p1]->get_postition()[2];
+      const double z2 = my_system->get_particle()[p1]->get_postition()[2];
+      const double sep  = (x1-x2+ y1-y2 +z1-z2)*(x1-x2+ y1-y2 +z1-z2);
+
+      rij = sqrt(sep);
+
+      const double Deriv       = -2*alpha*(2*x1+2*y1+beta*z1)
+      const double doubleDeriv = 4*alpha2*(x1*x1 + y1*y1 + beta*beta*z1*z1)-
+				 abterm;
+      const double u_mark      = -my_bosonSize/(2*rij*(my_bosonSize - sqrt(rij)));
+      const double u_2mark     = boson*(boson*(-boson + sqrt(r)) + 3*(boson - sqrt(r))**2)/(4*r**2*(boson - sqrt(r))**3) ;
+
+
+    }
+  }
+}
 
 
 
--- sympy/psi_w_corr.py
+++ sympy/psi_w_corr.py
@@ -1,11 +1,8 @@
 import sympy as sym
-from sympy.vector import CoordSysCartesian
 
 x,y,z,r,f,u,a,b,boson = sym.symbols('x y z r f u a b boson')
-C = CoordSysCartesian('C')
-r = x*C.i + y*C.j + z*C.k
 g = sym.functions.exp(-a*(x**2 + y**2 + b*z**2))
-f = 1 - boson/(sym.sqrt(x**2+y**2+z**2))
+f = 1 - boson/(sym.sqrt(r))
 u = sym.log(f)
 
 def nabla2():
@@ -14,15 +11,11 @@ def nabla2():
     print(g_2mark.simplify())
 
 def uder():
-    u_mark = sym.Derivative(u,x).doit() +\
-            sym.Derivative(u,y).doit() +\
-            sym.Derivative(u,z).doit()
+    u_mark = sym.Derivative(u,r).doit() 
 
-    u_2mark = sym.Derivative(u_mark,x).doit() +\
-            sym.Derivative(u_mark,y).doit() +\
-            sym.Derivative(u_mark,z).doit()
+    u_2mark = sym.Derivative(u_mark,r).doit()
 
     print(u_mark.simplify())
-    #print(u_2mark.simplify())
+    print(u_2mark.simplify())
 
 uder()
